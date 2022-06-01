a344ba65251c6ace8c47f2907e557ccffed59630
EldorKarimov <hacktivist19021@gmail.com>
2022-06-01 07:20:24 +0500

    some changes

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import QuizModel, Question, Answer, Result
 
-# Register your models here.
+@admin.register(QuizModel)
+class QuizModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {"slug":['name']}
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = ['name', 'is_right']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    fields = ['name', 'quiz']
+    inlines = [AnswerInlineModel]
+
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['number_of_question', 'correct_question', 'datetime']
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuizModel(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    quiz = models.ForeignKey(QuizModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Answer(models.Model):
+    name = models.CharField(max_length=128)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_right = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    number_of_question = models.IntegerField()
+    correct_question = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(QuizModel, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
